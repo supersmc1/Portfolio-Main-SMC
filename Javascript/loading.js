3976b46ce342d417c5a19c317da6094bcd1008dc
sunwanchung <sunwanchung@gmail.com>
2022-05-31 18:04:50 +0900

    합치는중

---
+++ Javascript/loading.js
@@ -0,0 +1,12 @@
+  //변수설정
+  const root = document.querySelector('#root')
+  const body = document.body
+  const roottwo = document.querySelector('.roottwo')
+  const bodychd = document.body.children
+  console.log(bodychd) 
+  console.dir(body)
+  //로딩 스크립트
+  setTimeout(function(){
+    root.remove() //5초뒤 root 삭제
+    roottwo.style.display="flex"//5초뒤 화면나오게 설정
+  },5000)
