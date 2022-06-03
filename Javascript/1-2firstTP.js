ea0ac9a76a47b32ae99dccd80eb59a299f16708b
sunwanchung <sunwanchung@gmail.com>
2022-06-03 13:11:52 +0900

    수정

--- Javascript/1-2firstTP.js
+++ Javascript/1-2firstTP.js
@@ -1,13 +1,13 @@
-const content = "WELCOME TO MY ELECTRONIC_DOCUMENT, \n THIS IS WEB-DESIGN \n I'M WEB-DESIGNER";
-const text = document.querySelector(".text");
-let indexx = 0;
+const content = "WELCOME TO MY ELECTRONIC_DOCUMENT, \n THIS IS WEB-DESIGN \n I'M WEB-DESIGNER"
+const text = document.querySelector(".text")
+let indexx = 0
 
 function typing(){
-  let txt = content[indexx++];
-  text.innerHTML += txt=== "\n" ? "<br/>": txt; 
+  let txt = content[indexx++]
+  text.innerHTML += txt=== "\n" ? "<br/>": txt
   if (indexx > content.length) {
-      text.textContent = "";
-      indexx = 0;
+      text.textContent = ""
+      indexx = 0
   }
 }
 setInterval(typing, 200)
---
+++ Javascript/2-second.js
@@ -0,0 +1,5 @@
+const desc = document.querySelector('.desc')
+const descchd = desc.children
+  for(let dsi = 0; dsi < descchd.length; dsi++){
+      descchd[dsi].style.margin = "20px"
+      }
