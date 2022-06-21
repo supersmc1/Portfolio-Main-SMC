98d9f3afbe28e41ba1dfb1e87eac82cd0c1a4591
sunwanchung <sunwanchung@gmail.com>
2022-06-21 17:59:31 +0900

    변경중

--- Javascript/2-second.js
+++ Javascript/2-second.js
@@ -2,4 +2,7 @@ const desc = document.querySelector('.desc')
 const descchd = desc.children
   for(let dsi = 0; dsi < descchd.length; dsi++){
       descchd[dsi].style.margin = "20px"
+      if(matchMedia("screen and (max-width: 500px)").matches){
+        descchd[dsi].style.marginTop = "5px"
+      }
       }
