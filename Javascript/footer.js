a3dee9126e21a57bd3ce7dadffcd5984eae17c33
sunwanchung <sunwanchung@gmail.com>
2022-06-23 00:59:54 +0900

    다시수정

--- Javascript/footer.js
+++ Javascript/footer.js
@@ -4,16 +4,6 @@ const cdplay = document.querySelector('.cdplay')
 const thirds = document.querySelector('.third')
 console.dir(cdplay)
 
-
-kakao.style.backgroundImage = 'url(../image/kakaos.jpg)'
-
-thirds.style.backgroundImage = 'url(../image/testthree.png)'
-
-cdplay.style.backgroundImage = 'url(../image/cdplayss.png)'
-
-
-github.style.backgroundImage = 'url(../image/github.png)'
-
 kakao.addEventListener('click',function(){
   alert('손만충의 kakao ID가 저장 되었습니다.')
 })
